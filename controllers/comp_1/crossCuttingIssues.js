c6ea7c64d1e27aeac988df8ddce8fb6fa685c4ee
dare babalola <darebabalola92@gmail.com>
2024-06-07 11:43:23 +0100

    lk

--- controllers/comp_1/crossCuttingIssues.js
+++ controllers/comp_1/crossCuttingIssues.js
@@ -81,7 +81,53 @@ router.get(
         return res.status(404).json('No cross cutting issue with sent Id')
       }
 
-      res.status(200).json({ success: true, data: existing_ccs })
+      const subDocs = existing_ccs[`${req.query.jurisdiction}_tc`]
+      const subDoc = subDocs.find(p => {
+        return p._id.toString() === req.params.id
+      })
+
+      res.status(200).json({ success: true, data: subDoc })
+    } catch (err) {
+      res.status(500).json({ success: false, message: err })
+    }
+  })
+)
+
+// PUT REQUESTS
+
+router.put(
+  '/update-specific-cross-cutting-issue/:id',
+  asyncErrCatcher(async (req, res) => {
+    try {
+      const items = req.body
+      const existing_ccs = await CCS.findOne({
+        [`${req.query.jurisdiction}_tc._id`]: req.params.id
+      })
+
+      if (!existing_ccs) {
+        return res.status(404).json('No cross cutting issue with sent Id')
+      }
+
+      const subDocs = existing_ccs[`${req.query.jurisdiction}_tc`]
+      const subDoc = subDocs.find(p => {
+        return p._id.toString() === req.params.id
+      })
+
+      for (const key in items) {
+        if (key in subDoc) {
+          for (const subKey in items[key]) {
+            if (subKey in subDoc[key]) {
+              subDoc[key][subKey] = items[key][subKey]
+            } else {
+              subDoc[key][subKey] = items[key][subKey]
+            }
+          }
+        }
+      }
+
+      await existing_ccs.save()
+
+      res.status(200).json({ success: true, subDoc })
     } catch (err) {
       res.status(500).json({ success: false, message: err })
     }
